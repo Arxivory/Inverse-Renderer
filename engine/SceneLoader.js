03ec7aa3839cfc830cdeba3267b0a7a24d25c2e9
Vince <babyaivan86@gmail.com>
2025-12-24 14:45:57 +0800

    Refactored scene loader.

--- engine/SceneLoader.js
+++ engine/SceneLoader.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { SceneState } from './SceneState';
+import { CameraEntity } from "./CameraEntity";
+import { LightEntity } from "./LightEntity";
 import { ObjectEntity } from './ObjectEntity';
 import { loadOBJMTL } from '../renderer/loaders';
 import { addToScene } from "../renderer/renderer";
@@ -8,7 +10,30 @@ export async function loadSceneFromJSON(path) {
     const response = await fetch(path);
     const data = await response.json();
 
-    SceneState.camera = data.camera;
+    SceneState.camera = new CameraEntity(data.camera);
 
-    
+    for (const lightDef of data.lights) {
+        const light = new LightEntity(lightDef);
+        SceneState.lights.push(light);
+        addToScene(light.light);
+    }
+
+    for (const objDef of data.objects) {
+        const mesh = await loadOBJMTL(
+            objDef.model.obj,
+            objDef.model.mtl
+        )
+
+        addToScene(mesh);
+
+        const objEntity = new ObjectEntity({
+            id: objDef.id,
+            mesh,
+            position: new THREE.Vector3(...objDef.transform.position),
+            rotation: new THREE.Vector3(...objDef.transform.rotation),
+            scale: new THREE.Vector3(...objDef.transform.scale)
+        });
+
+        SceneState.objects.push(objEntity);
+    }
 }
