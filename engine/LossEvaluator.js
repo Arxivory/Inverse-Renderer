adb3737aa0ff68632109d3c19af44a5f44327a62
Vince <babyaivan86@gmail.com>
2025-12-27 12:47:22 +0800

    Added functions for L1 and L2 Loss Computation.

--- engine/LossEvaluator.js
+++ engine/LossEvaluator.js
@@ -1,5 +1,49 @@
 import { referenceImageData } from "../utils/imageReferenceLoader.js";
 
+function computeL2(renderedPixels, n) {
+    let loss = 0;
+
+    for (let i = 0; i < n; i += 4) {
+        const r1 = renderedPixels[i] / 255;
+        const g1 = renderedPixels[i + 1] / 255;
+        const b1 = renderedPixels[i + 2] / 255;
+
+        const r2 = referenceImageData[i] / 255;
+        const g2 = referenceImageData[i + 1] / 255;
+        const b2 = referenceImageData[i + 2] / 255;
+
+        const dr = r1 - r2;
+        const dg = g1 - g2;
+        const db = b1 - b2;
+
+        loss += (dr * dr) + (dg * dg) + (db * db);
+    }
+
+    return loss / (n / 4);
+}
+
+function computeL1(renderedPixels, n) {
+    let loss = 0;
+
+    for (let i = 0; i < n; i += 4) {
+        const r1 = renderedPixels[i] / 255;
+        const g1 = renderedPixels[i + 1] / 255;
+        const b1 = renderedPixels[i + 2] / 255;
+
+        const r2 = referenceImageData[i] / 255;
+        const g2 = referenceImageData[i + 1] / 255;
+        const b2 = referenceImageData[i + 2] / 255;
+
+        const dr = r1 - r2;
+        const dg = g1 - g2;
+        const db = b1 - b2;
+
+        loss += Math.abs(dr) + Math.abs(dg) + Math.abs(db);
+    }
+
+    return loss / (n / 4);
+}
+
 export function computeLoss(renderedPixels) {
     if (!referenceImageData)
         throw new Error("Reference Image is not loaded");
